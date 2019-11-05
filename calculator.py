ae3b3c87daaa78be436352d9ccdbc38f4474ae93
Audi-ana Brown <audianab@gmail.com>
2019-11-04 23:54:48 -0600

    Used tkinter to give my calculator a GUI

--- calculator.py
+++ calculator.py
@@ -1,34 +1,115 @@
-def add(x, y):
-    return x + y
+from tkinter import *
 
-def subtract(x, y):
-    return x - y
 
-def multiply(x, y):
-    return x * y
+window = Tk()
+window.title("Simple Calculator")
 
-def divide(x, y):
-    return x / y
+e = Entry(window, width=35, borderwidth=5)
+e.grid(row=0, column =0, columnspan=4, padx=10, pady=10)
 
-print("Select operation.")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+def button_click(number):
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
 
-#take input from user
-choice = input("Enter choice(1/2/3/4):")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+def clear():
+   e.delete(0, END)
 
-if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
-elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
-elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
-elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Invalid input")
+
+def add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "divide"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def equal():
+    second_number = e.get() #this is gonna pull whatever is in the text box
+    e.delete(0, END)
+
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+    if math == "subtraction":
+        e.insert(0, f_num - int(second_number))
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+    if math == "division":
+        e.insert(0, f_num + int(second_number))
+
+
+
+
+
+
+
+#Define Buttons - because you can't pass a parameter in a button you have to use lambda 
+
+button_1 = Button(window, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(window, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(window, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(window, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(window, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(window, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(window, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(window, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(window, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(window, text="0", padx=39, pady=20, command=lambda: button_click(0))
+button_add = Button(window, text="+", padx=40, pady=20, command=add)
+button_subtract = Button(window, text="-", padx=40, pady=20, command=subtract)
+button_multiply = Button(window, text="x", padx=40, pady=20, command=multiply)
+button_divide = Button(window, text="÷", padx=40, pady=20, command=divide)
+button_equal = Button(window, text="=", padx=40, pady=20, command=equal)
+button_clear = Button(window, text="CE", padx=38, pady=20, command=clear)
+
+
+#Positioning the buttons the way you want them to be displayed
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+button_0.grid(row=4, column=1)
+
+button_equal.grid(row=4, column=2)
+button_clear.grid(row=4, column=0)
+
+
+button_add.grid(row=4, column=3)
+button_subtract.grid(row=3, column=3)
+button_multiply.grid(row=2, column=3)
+button_divide.grid(row=1, column=3)
+
+window.mainloop()
